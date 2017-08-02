e54e50f2101b6cb926feae809b893acdc604c870
Denis <nppteploautomat2000@gmail.com>
2017-08-02 15:51:49 +0300

    02/08

--- app/js/user.js
+++ app/js/user.js
@@ -1,15 +1,6 @@
-//$(document).ready(function(){
-//	$('.one-time').slick({
-//		dots: true,
-//		infinite: true,
-//		speed: 300,
-//		slidesToShow: 1,
-//		adaptiveHeight: true
-//	});
-//}); 
-
 $(document).ready(function() {
 	$('.searchButton').click(function() {
 		$('.search').toggleClass('active');
 	});
+	$('.single-item').slick();
 });
