bdd34fa742bf7e7584094c749e6674f35065d572
denisRyazanov <nppteploautomat2000@gmail.com>
2017-08-02 21:58:59 +0300

    02/08_02

--- app/js/user.js
+++ app/js/user.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
 	$('.searchButton').click(function() {
 		$('.search').toggleClass('active');
 	});
-	$('.single-item').slick();
+	$('.single-item').slick({
+		dots: true
+	});
 });
