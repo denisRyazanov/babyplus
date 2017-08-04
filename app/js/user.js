824b22637b7d0253486915c05bb61a569bfd6cb2
Denis <nppteploautomat2000@gmail.com>
2017-08-04 15:52:45 +0300

    04/08

--- app/js/user.js
+++ app/js/user.js
@@ -2,7 +2,27 @@ $(document).ready(function() {
 	$('.searchButton').click(function() {
 		$('.search').toggleClass('active');
 	});
-	$('.single-item').slick({
-		dots: true
+	$('.one-time').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	});
+});
+$(document).ready(function() {
+	$('.minus').click(function () {
+		var $input = $(this).parent().find('input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.plus').click(function () {
+		var $input = $(this).parent().find('input');
+		$input.val(parseInt($input.val()) + 1);
+		$input.change();
+		return false;
 	});
 });
